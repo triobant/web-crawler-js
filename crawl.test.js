c3d5fc3bd6ddcfd5ed3ad6a223c24364cf7a5486
Brian Ott <bott@b1-systems.de>
2024-08-23 11:19:28 +0200

    add: TDD approach - test to check if input URL gets normalized as expected

---
+++ crawl.test.js
@@ -0,0 +1,10 @@
+import { normalizeURL } from "./crawl.js";
+import { test, expect } from "@jest/globals";
+
+
+test('normalizeURL protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
