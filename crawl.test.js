54ed22c4cff4914f1297c0452e530ff14ae6bd11
Brian Ott <bott@b1-systems.de>
2024-08-23 11:22:25 +0200

    add: tests to check if slash at the end, capitals and http also get normalized

--- crawl.test.js
+++ crawl.test.js
@@ -8,3 +8,24 @@ test('normalizeURL protocol', () => {
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
+
+test('normalizeURL slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL http', () => {
+    const input = 'http://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
