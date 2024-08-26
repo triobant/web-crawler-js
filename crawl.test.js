0d25d473928791df8364659a92d3d07e9d706513
Brian Ott <bott@b1-systems.de>
2024-08-26 09:12:23 +0200

    update: more tests to check if getURLsFromHTML works as expected

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
 import { test, expect } from "@jest/globals";
 
 
@@ -27,5 +27,29 @@ test('normalizeURL http', () => {
     const input = 'http://BLOG.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href ="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/' ]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href ="/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/path/one' ]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href ="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/path/one', 'https://other.com/path/one' ]
     expect(actual).toEqual(expected)
 })
