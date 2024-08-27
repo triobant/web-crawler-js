4360e0919915d6be7a7149d99f5c165a418e4242
Brian Ott <bott@b1-systems.de>
2024-08-27 09:06:41 +0200

    update: main func to get the cli arg

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+import { argv } from 'node:process'
+
 function main() {
-    // ?
+    if (argv.length < 3) {
+        console.log("not enough args")
+    }
+    if (argv.length > 3) {
+        console.log("too many args")
+    }
+    const baseURL = argv[2]
+
+    console.log(`starting crawl of: ${baseURL}...`)
 }
 
 main()
