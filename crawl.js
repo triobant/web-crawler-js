15daac967b84392f47d09d91c1794c7eb1361cb7
Brian Ott <bott@b1-systems.de>
2024-08-30 08:35:09 +0200

    update: crawlPage to check if pages were already visited and get the next url

--- crawl.js
+++ crawl.js
@@ -52,28 +52,47 @@ async function fetchHTML(url) {
 }
 
 // use default args to prime the first call
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+    // if this is an offsite URL, bail immediately
+    const currentURLObj = new URL(currentURL)
+    const baseURLObj = new URL(baseURL)
+    if (currentURL.hostname !== baseURLObj.hostname) {
+        return pages
+    }
+
+    // use a consistent URL format
+    const normalizedURL = normalizeURL(currentURL)
+
+    // if we've already visited this page
+    // just increase the count and don't repeat
+    // the http request
+
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++
+        return pages
+    }
+
+    // initialize this page in the map
+    // since it doesn't exist yet
+    pages[normalizedURL] = 1
+
+    // fetch and parse the html of the currentURL
     console.log(`crawling ${currentURL}`)
-
-    let res
+    let html = ''
     try {
-        res = await fetch(currentURL)
+        html = await fetchHTML(currentURL)
     } catch (err) {
-        throw new Error(`Got Network error: ${err.message}`)
+        console.log(`${err.message}`)
+        return pages
     }
 
-    if (res.status > 399) {
-        console.log(`Got HTTP error: ${res.status} ${res.statusText}`)
-        return
+    // recur through the page's links
+    const nextURLs = getURLsFromHTML(html, baseURL)
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURLs, pages)
     }
 
-    const contentType = res.headers.get('content-type')
-    if (!contentType || !contentType.includes('text/html')) {
-        console.log(`Got non-HTML response: ${contentType}`)
-        return
-    }
-
-    console.log(await res.text())
+    return pages
 }
 
 export { normalizeURL, getURLsFromHTML, crawlPage }
